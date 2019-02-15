a8fb4a073ddaea45168bff9b3c5ba2a4ce1c7e47
Romain Tartière <romain@blogreen.org>
2019-02-15 07:41:03 -1000

    Rename to tls-checker
    Finding names is hard.
    
    Rename riemann_tls_checker to tls-checker to align with
    certificate-checker which tests the certificates without connexions.

--- lib/riemann_tls_checker.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-require 'riemann_tls_checker/certificate_checker'
-require 'riemann_tls_checker/certificate_checker_factory'
-require 'riemann_tls_checker/line_oriented_socket'
-require 'riemann_tls_checker/now'
---
+++ lib/tls-checker.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+require 'tls-checker/certificate_checker'
+require 'tls-checker/certificate_checker_factory'
+require 'tls-checker/line_oriented_socket'
+require 'tls-checker/now'
